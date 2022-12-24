59a6394b0b3a6aa8cbb10e37851d5be6c627c2d0
ShinDongHyeok2709 <donghyeok.shin@gmail.com>
2022-12-24 12:01:28 +0900

    Header % 반영

--- todos/src/TodoHeader.jsx
+++ todos/src/TodoHeader.jsx
@@ -4,13 +4,16 @@ import useTodosContext from "./hooks/useTodosContext";
 export default function TodoHeader() {
   const [todos] = useTodosContext();
   const todoCount = todos.filter((todo) => todo.done === false).length;
+
   return (
     <>
       <h1>나의 할일(Todo) 목록</h1>
       <p>
         <b>할일 : </b>
-        <span style={{ color: "red" }}>{todoCount}</span>개
-        <span>&nbsp;({todos.length}개)</span>
+        <span style={{ color: "red" }}>{todoCount}</span>
+        <span>/{todos.length}개</span>
+        &nbsp;(
+        {Math.round((todoCount / todos.length) * 100)}%)
       </p>
     </>
   );
